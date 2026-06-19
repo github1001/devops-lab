46d538d248f08fb8b5bee98d0b981d89688d6327
Logan <logan_TM@hotmail.com>
2026-06-19 17:30:16 +0000

    Use APP_ENV environment variable

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
-const env = process.env.APP_ENV || "dev";
+const env = process.env.APP_ENV || "local";
 
 app.get("/", (req, res) => {
   res.json({
